dcbb1c434b9e6271c2aaf950db0dfd0437ee16f3
Matheus Wallace da Silva Pereira <matheus.wallace1@outlook.com>
2024-08-04 20:57:27 -0300

    feat: import of components sidebar, resumo and dataContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import Header from './components/Header/Header';
+import Sidenav from './components/Sidenav/Sidenav';
+import { DataContextProvider } from './Context/DataContext';
+import Resumo from './Pages/Resumo';
 import './style.css';
 
 function App() {
-  return <div>oi</div>;
+  return (
+    <DataContextProvider>
+      <div>
+        <Sidenav />
+        <main>
+          <Header />
+          <Resumo />
+        </main>
+      </div>
+    </DataContextProvider>
+  );
 }
 
 export default App;
